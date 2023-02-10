01a52072700f608d10c25a81edaf33b234fe64a0
Evgenii Urvantsev <resmus@gmail.com>
2023-02-10 15:01:01 +0300

    impl binary data in json formatter, fixing liveness handler, add context

--- formatter/json.go
+++ formatter/json.go
@@ -16,7 +16,33 @@ func NewJsonPretty() *Json {
 	return &Json{pretty: true}
 }
 
+type BinaryData struct {
+	data        []byte
+	contentType string
+	fileName    string
+}
+
+func NewBinaryData(data []byte, mimeType string) *BinaryData {
+	if len(mimeType) == 0 {
+		mimeType = "application/octet-stream"
+	}
+	return &BinaryData{data: data, contentType: mimeType}
+}
+
 func (j *Json) Write(w http.ResponseWriter, statusCode int, data any) error {
+	if bt, ok := data.(BinaryData); ok {
+		w.Header().Set("Content-Type", bt.contentType)
+		if len(bt.fileName) > 0 {
+			w.Header().Set(
+				"Content-Disposition",
+				`attachment; filename="`+bt.fileName+`"`,
+			)
+		}
+		w.WriteHeader(statusCode)
+		_, err := w.Write(bt.data)
+		return err
+	}
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
 
